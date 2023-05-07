81ab8b0d2ddf0456c8516732e90942c80a45db4f
michael <michaelutoh21@gmail.com>
2023-05-07 23:56:20 +0100

    Add products table

--- core/models/products.py
+++ core/models/products.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
@@ -5,3 +6,8 @@ from sqlmodel import Field, SQLModel
 
 class Product(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    product_no: str
+    description: str
+    created_timestamp: Optional[datetime] = Field(default=datetime.utcnow())
+    updated_timestamp: Optional[datetime] = Field(default=datetime.utcnow())
